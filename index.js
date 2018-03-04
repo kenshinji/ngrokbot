c596ca6fc83a3ff0ccbfae98dd770f804bf8217c
kenshinji <gundam0083ster@gmail.com>
2018-03-04 14:31:58 +0000

    Fix response format

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
+#!/usr/bin/env node
 const request = require('request');
 const Telegraf = require('telegraf');
 const NGROK_URL = 'http://127.0.0.1:4040/api/tunnels';
-const TOKEN = ''
+const TOKEN = require('./config.json').token; 
 const app = new Telegraf(TOKEN);
 app.command('start', ({ from, reply }) => {
   console.log('start', from)
   return reply('Welcome!')
 })
 app.hears('ngrok URL', (ctx) => {
-  // ctx.reply('Hey there!')
   request(NGROK_URL, function (error, response, json) {
     json = JSON.parse(json);
-    ctx.reply(json["tunnels"]["0"]["public_url"]);
+    let tunnels = [];
+    if(Array.isArray(json.tunnels)){
+      tunnels = json.tunnels.map(e => e.name + " : " + e.public_url);
+    }
+      ctx.reply(tunnels);
   });
 })
 app.startPolling()
